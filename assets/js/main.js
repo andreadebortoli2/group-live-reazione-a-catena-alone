f27d99edc339ae5ddaa528886723253cf3731f47
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-03-09 23:28:37 +0100

    edit removed not functional code and edited the syntax

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 // console.log('test');
-
-/* generate an object */
+/* class declaration function contructor */
 class item {
     constructor(image, name, quantity, price) {
         this.image = image;
@@ -19,20 +18,7 @@ const products = [
     frenchFries = new item('french-fries.png', 'French Fries', 1, 5.50),
     kebab = new item('kebab.png', 'Kebab', 1, 7.00),
     pizza = new item('pizza.png', 'Pizza', 1, 10.00),
-
 ];
-
-
-
-/* let cartTotalCost = 0; */
-
-
-
-
-document.querySelector('.items_counter').innerHTML = `${products.length} Items`;
-
-const tableTbody = document.querySelector('tbody');
-// console.log(tableTbody);
 
 /**
  * print on page a product as a row in the table
@@ -41,40 +27,14 @@ const tableTbody = document.querySelector('tbody');
 */
 function generateItem(item) {
 
-    /* const tableRowMarkup =
-        `<tr class="item_row">
-            <td class="item_title">
-                 <img class="item_img" src="./assets/img/foods/${item.image}" alt="">
-                <!-- /.item_img -->
-                 <div class="item_text">
-                    <div class="item_name">${item.name}</div>
-                     <!-- /.item_name -->
-                     <div class="item_remove">Remove</div>
-                    <!-- /.remove_item -->
-                 </div>
-                <!-- /.item_text -->
-            </td>
-            <!-- /.item_title -->
-            <td class="quantity">
-                <button class="minus_button">&minus;</button>
-                 <div class="quantity_counter">${item.quantity}</div>
-                <button class="plus_button">&plus;</button>
-            </td>
-            <!-- /.quantity -->
-            <td>${item.price}€</td>
-            <td>${item.totalPrice()}€</td>
-        </tr>
-        <!-- /.item_row -->`;
-
-    tableTbody.insertAdjacentHTML('beforeend', tableRowMarkup); */
-
-
+    // create table row for the item
     const trItemRow = document.createElement('tr');
     tableTbody.append(trItemRow);
 
+    
     // item title
     const tdItemTitle = document.createElement('td');
-    tdItemTitle.classList.add('item_title')
+    tdItemTitle.classList.add('item_title');
     tdItemTitle.innerHTML =
         `<img class="item_img" src="./assets/img/foods/${item.image}" alt="">
     <!-- /.item_img -->
@@ -86,25 +46,17 @@ function generateItem(item) {
     </div>
     <!-- /.item_text -->`;
 
+
     // item quantity
     const tdItemQuantity = document.createElement('td');
     tdItemQuantity.classList.add('quantity');
 
 
-    //+++++++++++++++++ todo bug minus sometimes stop to work if you click minus one more time after reach the 0 ++++++++++++++++
     // --button minus
     const minusButton = document.createElement('button');
     minusButton.classList.add('minus_button');
     minusButton.innerHTML = '-';
-    /* minusButton.addEventListener('click', function(e) {
-        item.quantity--
-        console.log(item.quantity);
-        quantityCounter.innerHTML = `${item.quantity}`;
-        tdItemTotal.innerHTML = `${item.totalPrice()}€`;
-        if (item.quantity === 0) {
-            this.removeEventListener('click', function)
-        }
-    }); */
+    //+++++++++++++++++ todo bug minus sometimes stop to work if you click minus one more time after reach the 0 ++++++++++++++++
     minusButton.addEventListener('click', subtract);
     function subtract() {
         if (item.quantity > 0) {
@@ -113,14 +65,16 @@ function generateItem(item) {
             quantityCounter.innerHTML = `${item.quantity}`;
             tdItemTotal.innerHTML = `${item.totalPrice()}€`;
         } else if (item.quantity < 1) {
-            this.removeEventListener('click', subtract)
+            this.removeEventListener('click', subtract);
         };
     };
+
 
     // --quantity counter
     const quantityCounter = document.createElement('div');
     quantityCounter.classList.add('quantity_counter');
     quantityCounter.innerHTML = `${item.quantity}`;
+
 
     // --button plus
     const plusButton = document.createElement('button');
@@ -134,16 +88,17 @@ function generateItem(item) {
 
         // sum the cart total cost
         products.forEach((product) => {
-            cartTotalCost = cartTotalCost + product.totalPrice()
-
-        })
+            cartTotalCost = cartTotalCost + product.totalPrice();
+        });
     });
 
+    // -- add quantity buttons and counter
     tdItemQuantity.append(minusButton, quantityCounter, plusButton);
+
 
     // item price and total
     const tdItemPrice = document.createElement('td');
-    tdItemPrice.classList.add('item_price')
+    tdItemPrice.classList.add('item_price');
     tdItemPrice.innerHTML = `${item.price}€`;
 
     const tdItemTotal = document.createElement('td');
@@ -156,22 +111,17 @@ function generateItem(item) {
 };
 
 
+
+// -- create the cart table
+document.querySelector('.items_counter').innerHTML = `${products.length} Items`;
+
+const tableTbody = document.querySelector('tbody');
+// console.log(tableTbody);
+
+
+
+// -- fill thecart table with items
 products.forEach(product => generateItem(product));
-
-
-/* products.forEach((product, index) => {
-    
-    generateItem(product);
-    
-    console.log(document.querySelectorAll('.plus_button')[index]);
-    document.querySelectorAll('.plus_button')[index].addEventListener('click', function () {
-        product.quantity++
-        console.log(product.quantity);
-        console.log(products[index]);
-        generateItem(products[index]);
-
-    });
-}); */
 
 //++++++++++++++++++++++++ sidebar ++++++++++++++++++++++++++
 
@@ -213,7 +163,7 @@ shippingCost.addEventListener('change', function () {
 
 // -- set 'observer' to work at all the changes on products.totalPrice and re-print cart summary price and checkot price --
 const observerCart = new MutationObserver(() => {
-    cartTotalCost = 0
+    cartTotalCost = 0;
     products.forEach(product => cartTotalCost = cartTotalCost + product.totalPrice());
     /* console.log(cartTotalCost); */
     cartSummaryPrice.innerHTML = `${cartTotalCost.toFixed(2)} €`;
@@ -223,5 +173,5 @@ const observerCart = new MutationObserver(() => {
 document.querySelectorAll('.item_total_price').forEach((item) => {
     observerCart.observe(item, {
         childList: true,
-      });
+    });
 });
